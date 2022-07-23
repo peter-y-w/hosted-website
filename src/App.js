1158a6fa71b13b9790b4751adcb6481605eef4f6
Peter <inbox.peterw@gmail.com>
2022-07-23 16:32:48 +1000

    init commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const App = () => {
+	return (
+		<Box sx={{ display: 'flex' }}>
+			<CssBaseline />
+			<AppBar>
+				<Toolbar>
+					<Typography variant="h6" noWrap component="div">
+						Hi Liv and Zacc
+					</Typography>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
+};
+
+export default App;
